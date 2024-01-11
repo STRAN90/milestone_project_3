1492fc2fd81bf8be3a9474c5b59c49741d6d07fc
Sonia Majewska <sonianltran@gmail.com>
2024-01-11 14:32:47 +0000

    Created models, css, js files, static folder and materialize link

---
+++ allergywisefood/models.py
@@ -0,0 +1,22 @@
+from allergywisefood import db
+
+
+class Catergory(db.Model):
+    # schema for Category Model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(30), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
+
+class Users(db.Model):
+    # schema for User Model 
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __rep__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.user_name
--- allergywisefood/routes.py
+++ allergywisefood/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from allergywisefood import app, db 
-
+from allergywisefood.models import Category, Users
 
 @app.route("/")
 def home():
