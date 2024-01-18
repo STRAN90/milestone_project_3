d16e451c1ce44c9ff74a1a9a5b623a21bafad5b5
Sonia Majewska <sonianltran@gmail.com>
2024-01-18 13:02:46 +0000

    Added extend base.html to html files. Added register template

--- allergywisefood/routes.py
+++ allergywisefood/routes.py
@@ -5,3 +5,11 @@ from allergywisefood.models import Category, Users
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
